32cf3bda51032cd4edf27bc89107fe63d318dd98
Lucasmq <lucasmedeirosq@gmail.com>
2020-01-15 06:39:09 -0300

    Atualizando API para retornar novos dados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 // express-async-errors tem que vim antes do import das routes
@@ -23,6 +24,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
